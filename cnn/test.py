108c04006a0c48b1ef5698e4be8ef3cc1728ebbf
ウメとタク <64407693+Cz1544252489@users.noreply.github.com>
2024-08-24 11:48:07 +0800

    Update test.py

--- cnn/test.py
+++ cnn/test.py
@@ -82,7 +82,7 @@ def infer(test_queue, model, criterion):
 
   for step, (input, target) in enumerate(test_queue):
     input = Variable(input, volatile=True).cuda()
-    target = Variable(target, volatile=True).cuda(async=True)
+    target = Variable(target, volatile=True).cuda(device=None, non_blocking=False)
 
     logits, _ = model(input)
     loss = criterion(logits, target)
